2ed05f11bf7f093357f00d5b1f6d45cbb3aef275
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-12 15:17:11 -0700

    working code to locate how many times nerves are mentioned and where they appear in each chapter that they appear

--- index.js
+++ index.js
@@ -16,11 +16,40 @@ var store_content_in_orchestrate_collection = function(i){
 	return store_content_in_orchestrate_collection;
 }
 // store_content_in_orchestrate_collection();
-var chapterName;
-for(var i = 1; i<10;i++){
-	var j = '0'+ i;
-	store_content_in_orchestrate_collection(j)
+function store_all_chapters_content(){
+	var chapterName;
+	for(var i = 1; i<10;i++){
+		var j = '0'+ i;
+		store_content_in_orchestrate_collection(j)
+	}
+	for(var i = 10; i<=61; i++){
+		store_content_in_orchestrate_collection(i);
+	}
 }
-for(var i = 10; i<=61; i++){
-	store_content_in_orchestrate_collection(i);
+
+function findNerves(chapter){
+	// var nerves_array
+	// console.log(str.match('/nerves/g').length);
+	return chapter.match(/nerves/g).length
 }
+
+db.search('pride_and_prejudice', 'value.contents: "nerves*"')
+.then(function(result){
+	var chapterText;
+	var nerveOccurs = {};
+	var chapterArray = result.body.results; 
+	for(chapterCount = 0;chapterCount<chapterArray.length;chapterCount++){
+		chapterText = chapterArray[chapterCount].value.contents;
+		var chapterNumber = chapterArray[chapterCount].value.chapter_number;
+		var count = findNerves(chapterText);
+		nerveOccurs[chapterNumber]= count;
+	}
+	console.log(nerveOccurs);
+}).fail(function(err){console.log('error');})
+
+
+
+
+// db.search('pride_and_prejudice', 'value.contents: "Mary" NOT "Lydia"')
+
+
